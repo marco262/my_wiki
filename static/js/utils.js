150436db9d0e65ab1ff8804ec7646661bc2b073f
marco262 <marco262@gmail.com>
2019-11-14 07:59:26 -0800

    added optional parameters to spell table

--- static/js/utils.js
+++ static/js/utils.js
@@ -15,31 +15,40 @@ export function title_case(s) {
     return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
 }
 
-export function spell_table(spells) {
-    // spells is list of 2-tuples of (url, dict)
+export function spell_table(spells, split=true, show_classes=true) {
+    // If split is False, spells is list of 2-tuples of (url, dict)
+    // If split is True, spells is a dictionary of spells by spell level, with each dict value taking the format above.
     return `
     <table border='1'>
-        ${spell_table_header()}
-        ${spells.map(spell => spell_table_row(spell[0], spell[1])).join("\n")}
+        ${spell_table_header(show_classes)}
+        ${spells.map(spell => spell_table_row(spell[0], spell[1], show_classes)).join("\n")}
     </table>`;
 }
 
-export function spell_table_header() {
-    return `
+export function spell_table_header(show_classes=true) {
+    let html = `
     <tr>
         <th>Spell Name</th>
         <th>School</th>
-        ${class_list.map(classname => `<th>${title_case(classname)}</th>`).join("\n")}
-        <th>Source</th>
+        `;
+    if (show_classes)
+        html += class_list.map(classname => `<th>${title_case(classname)}</th>`).join("\n");
+    html += `
+        <th>Source</th>;
     </tr>`;
+    return html;
 }
 
-export function spell_table_row(url, dict) {
-    return `
+export function spell_table_row(url, dict, show_classes=true) {
+    let html =  `
     <tr>
         <td><a href="spell/${url}">${dict["title"]}</a></td>
         <td>${title_case(dict["school"])}</td>
-        ${class_list.map(classname => `<td>${dict["classes"].includes(classname) ? "X" : ""}</td>`).join("\n")}
+        `;
+    if (show_classes)
+        html += class_list.map(classname => `<td>${dict["classes"].includes(classname) ? "X" : ""}</td>`).join("\n");
+    html += `
         <td>${dict["source"]}</td>
     </tr>`;
+    return html;
 }
