aaa36745d15e6958840d185eeb6c3ffe87603bfd
marco262 <marco262@gmail.com>
2019-11-30 09:39:27 -0800

    Added support for either templates or markdown

--- src/endpoints/numenera.py
+++ src/endpoints/numenera.py
@@ -1,5 +1,6 @@
 import re
 from html import unescape
+from os.path import isfile
 
 from bottle import get, view, HTTPError, template
 
@@ -20,20 +21,16 @@ def load_wsgi_endpoints():
         md = MD.parse_md_path("data/numenera/home.md")
         return {"title": "Numenera", "text": md, "toc": md.toc_html}
 
-    @get('/numenera/Mutants_new')
-    @view("numenera/page.tpl")
-    def mutants():
-        t = unescape(template("numenera/mutants.tpl"))
-        md = MD.parse_md(t)
-        return {"title": "Mutants", "text": md, "toc": md.toc_html}
-
     @get('/numenera/<name>')
     @view("numenera/page.tpl")
     def page(name):
         formatted_name = re.sub("\W", "-", name.lower())
-        path = "data/numenera/" + formatted_name + ".md"
-        try:
-            md = MD.parse_md_path(path)
-        except FileNotFoundError:
+        if isfile(f"views/numenera/{formatted_name}.tpl"):
+            text = unescape(template(f"views/numenera/{formatted_name}.tpl"))
+        elif isfile(f"data/numenera/{formatted_name}.md"):
+            with open(f"data/numenera/{formatted_name}.md") as f:
+                text = f.read()
+        else:
             raise HTTPError(404, f"I couldn't find \"{name}\".")
+        md = MD.parse_md(text)
         return {"title": name.title(), "text": md, "toc": md.toc_html}
