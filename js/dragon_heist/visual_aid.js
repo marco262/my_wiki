2f6aebb68a8e7f41ede84538da4328adf79bc7e3
marco262 <marco262@gmail.com>
2020-05-24 13:20:32 -0700

    Generalized loading websockets

--- js/dragon_heist/visual_aid.js
+++ js/dragon_heist/visual_aid.js
@@ -1,3 +1,4 @@
+let load_websockets = true;
 let websocket_errors = 0;
 let max_websocket_errors = 3;
 let last_url = "";
@@ -19,23 +20,29 @@ function load_websocket(url, func) {
     ws_uri += `//${loc.host}/dragon_heist/${url}`;
     let ws = new WebSocket(ws_uri);
     ws.onmessage = func;
-    ws.onerror = on_websocket_error;
+    ws.onerror = error => on_websocket_error(url, func, error);
     ws_dict[url] = ws;
+    console.log(`Loaded websocket for "${url}"`)
 }
 
-function on_websocket_error(error) {
+function on_websocket_error(url, func, error) {
+    if (!load_websockets) {
+        return;
+    }
     console.log("WebSocket error:");
     console.log(error);
     websocket_errors += 1;
     if (websocket_errors >= max_websocket_errors) {
+        load_websockets = false;
         document.getElementById("page").hidden = true;
         let error_msg = document.getElementById("error-message");
-        error_msg.innerText = `Failed to connect to WebSocket after ${websocket_errors} attempts. Please reload page to try again.`;
+        error_msg.innerText = `Failed to connect to WebSocket "${url}" after ${websocket_errors} attempts. ` +
+            `Please reload page to try again.`;
         error_msg.hidden = false;
-        return
+        return;
     }
     console.log("Reconnecting in 5 seconds...");
-    setTimeout(load_websocket, 5000);
+    setTimeout(() => load_websocket(url, func), 5000);
 }
 
 function handle_visual_aid(msg) {
