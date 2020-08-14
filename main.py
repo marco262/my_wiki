c5a7901bc49ff69080ff826ceb7168b5b32b866f
marco262 <marco262@gmail.com>
2020-08-14 14:37:40 -0700

    Added watchdog to server

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from argparse import ArgumentParser
+from time import sleep
 
 from src.server import Server
 
 parser = ArgumentParser()
 parser.add_argument('-d', '--debug', action="store_true")
+parser.add_argument('-w', '--disable-watchdog', action="store_true")
 args = parser.parse_args()
 
-Server(debug=args.debug)
+ENABLE_WATCHDOG = not args.disable_watchdog
+
+while True:
+    try:
+        Server(debug=args.debug)
+    except KeyboardInterrupt:
+        if not ENABLE_WATCHDOG:
+            break
+    sleep(10)
