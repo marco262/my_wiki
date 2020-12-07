34f3e44d93331b7e194064fa3d539c480ff41ae3
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-12-06 21:03:04 -0800

    Added Major magic items

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -4,5 +4,5 @@ from src.dnd.utils import get_magic_item_table
 
 os.chdir("..")
 
-for k, v in get_magic_item_table("Minor", "Legendary").items():
+for k, v in get_magic_item_table("Major", "Legendary").items():
     print(k, v)
