b65f2aa0b5495807a99d783b518d12ff5005eed3
marco262 <marco262@gmail.com>
2020-12-06 18:19:36 -0800

    Added Minor Rare, Very Rare, and Legendary item tables

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -4,5 +4,5 @@ from src.dnd.utils import get_magic_item_table
 
 os.chdir("..")
 
-for k, v in get_magic_item_table("Minor", "Uncommon").items():
+for k, v in get_magic_item_table("Minor", "Legendary").items():
     print(k, v)
