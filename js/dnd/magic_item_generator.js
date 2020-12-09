a0055124963dcac1c02157c7278d5b5c1121331f
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2020-12-08 20:26:50 -0800

    Added customization to magic items generator

--- js/dnd/magic_item_generator.js
+++ js/dnd/magic_item_generator.js
@@ -3,7 +3,15 @@ import { ajax_call } from "../common/utils.js";
 export function generate() {
     let table_name = document.getElementById("magic-item-tables").value;
     if (table_name === "") return;
-    ajax_call("/dnd/magic_item_generator_results/" + table_name, handle_generator_results);
+    let max_items = parseInt(document.getElementById("max_items").value);
+    let no_duplicates = document.getElementById("no_duplicates").checked;
+    let params = {"table_name": table_name, "max_items": max_items, "no_duplicates": no_duplicates};
+    console.log(params);
+    ajax_call(
+        "/dnd/magic_item_generator_results/", 
+        handle_generator_results, 
+        params
+    );
 }
 
 function handle_generator_results(xhttp) {
