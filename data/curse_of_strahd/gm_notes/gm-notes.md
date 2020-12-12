5df9c2807b80153aebac674ed5839dbe22fbf5ea
marco262 <marco262@gmail.com>
2020-12-12 11:58:44 -0800

    Added initial Curse of Strahd notes

---
+++ data/curse_of_strahd/gm_notes/gm-notes.md
@@ -0,0 +1,22 @@
+<script type="module">
+    import {init_links, init_visual_aid} from "/js/common/visual_aid_backend.js";
+    init_links();
+    init_visual_aid();
+</script>
+
+* [Notebook](Notebook)
+* [Calendar](/curse_of_strahd/calendar)
+* [Soundboard](Soundboard)
+
+Enter another URL to set as Visual Aid here:
+
+<input type="text" id="custom_visual_aid_url"> <button id="custom_visual_aid_button">Visual Aid</button>
+
+[Visual aid](/dragon_heist/visual_aid)
+
+# Daggerford
+
+* [Sword Coast](^curse_of_strahd/the_sword_coast.jpg) ([Daggerford](^curse_of_strahd/the_sword_coast_daggerford.jpg))
+* [Daggerford](^curse_of_strahd/Daggerford.jpg), [map 1](^curse_of_strahd/Daggerford-Map-Forgotten-Realms.jpg), [map 2](^curse_of_strahd/Daggerford-Side-View-Expanded-Map-Forgotten-Realms.gif), [more pictures](https://medium.com/@7h3d34dh4nd/session-one-daggerford-the-road-nightstone-b06308cddf8d)
+* [Lord Zelraun Roaringhorn](^curse_of_strahd/Lord_Zelraun_Roaringhorn.jpg)
+* [Duchess Morwen of Daggerford](^curse_of_strahd/duchess-morwen.jpg)
---
+++ src/curse_of_strahd/endpoints.py
@@ -0,0 +1,39 @@
+import bcrypt
+from bottle import Bottle, view, auth_basic
+
+from src.common.utils import md_page
+
+# Default password: dancinglikeastripper
+GM_NOTES_PW_HASH = b"$2b$12$CQk/8o5DPPy05njxM8kO4e/WWr5UV7EXtE1sjctnKAUCLj5nqTcHC"
+
+
+def init(cfg):
+    global GM_NOTES_PW_HASH
+    GM_NOTES_PW_HASH = cfg.get("Password hashes", "GM Notes").encode("utf-8")
+
+
+def load_wsgi_endpoints(app: Bottle):
+    @app.get("/")
+    @app.get("/home")
+    def home():
+        return md_page("Curse of Strahd", "curse_of_strahd")
+
+    @app.get("<name>")
+    @view("common/page.tpl")
+    def page(name):
+        return md_page(name, "curse_of_strahd")
+
+    @app.get("gm_notes/<name>")
+    @view("common/page.tpl")
+    @auth_basic(gm_notes_auth_check)
+    def gm_notes(name):
+        return md_page(name, "curse_of_strahd", directory="gm_notes")
+
+    @app.get("")
+    @view("common/page.tpl")
+    def calendar():
+        return md_page("calendar", "curse_of_strahd", build_toc=False)
+
+
+def gm_notes_auth_check(username, password):
+    return username.lower() == "gm" and bcrypt.checkpw(password.encode("utf-8"), GM_NOTES_PW_HASH)
