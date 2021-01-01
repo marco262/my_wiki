b86f3d1f0cc585ec222845095937cfc1fbf22970
marco262 <marco262@gmail.com>
2021-01-01 10:37:17 -0800

    Adjusted volume on some audio files

---
+++ scripts/fix_audio_file_names.py
@@ -0,0 +1,14 @@
+import re
+
+import os
+from glob import glob
+
+os.chdir("../static/audio/curse_of_strahd")
+
+for filepath in glob("*.mp3"):
+    # Replace unallowed characters with spaces
+    filename = re.sub(r"[^A-Za-z0-9_'.&]", " ", filepath)
+    # Collapse multiple spaces to one
+    filename = re.sub(r" +", " ", filename)
+    filename = filename.replace("D D", "D&D")
+    os.rename(filepath, filename)
