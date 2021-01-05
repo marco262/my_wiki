5b004238fe6002998009340ab3f3c394a0ccd7f2
marco262 <marco262@gmail.com>
2021-01-05 07:02:37 -0800

    Simplify main.py

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ parser.add_argument('-d', '--debug', action="store_true")
 parser.add_argument('-w', '--disable-watchdog', action="store_true")
 args = parser.parse_args()
 
-ENABLE_WATCHDOG = not args.disable_watchdog
-
 while True:
     try:
         Server(debug=args.debug)
     except KeyboardInterrupt:
-        print("To manually kill the server, send another interrupt in the next 10 seconds.")
-        if not ENABLE_WATCHDOG:
+        if args.disable_watchdog:
             break
-    sleep(10)
+        print("To manually kill the server, send another interrupt in the next 10 seconds.")
+        sleep(10)
