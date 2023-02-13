8f4374d1d5b9a1f4951848b49debbba25baa5cc3
marco262 <marco262@gmail.com>
2023-02-13 13:32:06 -0800

    Added spells

--- scripts/fix_selected_text.py
+++ scripts/fix_selected_text.py
@@ -57,12 +57,12 @@ elif sys.argv[1] == "add_special_formatting":
             title = title.replace(n.group(0), f"({n.group(1).upper()}")
         text = text.replace(m.group(0), f'{title},')
 elif sys.argv[1] == "feats":
-    text = text.replace("\r\n", "|")
-    for m in re.finditer(r"([ A-Z:]+)\|(.*?)-Level Feat\|Prerequisite: (.*?)\|Repeatable: (.*?)\|", text, re.MULTILINE):
+    text = text.replace("\r?\n", "|")
+    for m in re.finditer(r"([ A-Z:]+)\|(.*?)-Level Feat\|Prerequisite: (.*?)\|", text, re.MULTILINE):
         # print(m.groups())
         name = m.group(1)
         name = name.replace(" ", "").title()
-        header_text = f"## {name}@@|*{m.group(2)}-Level Feat*@@|**Prerequisite:** {m.group(3)}  @|**Repeatable:** {m.group(4)}@@|"
+        header_text = f"## {name}@@|*{m.group(2)}-Level Feat*@@|**Prerequisite:** {m.group(3)}  @@|"
         text = text.replace(m.group(0), header_text)
     text = text.replace("|", "\n")
 elif sys.argv[1] == "create_ability_list":
--- scripts/wiki_to_toml_spell.py
+++ scripts/wiki_to_toml_spell.py
@@ -1,99 +1,107 @@
+import re
 from glob import glob
+from json import dumps
 from os.path import basename
 from re import search
 
-for path in glob("../data/spell/*"):
+
+source = "Cleric and Revised Species"
+
+for path in glob("../data/onednd/spell/*"):
     print(path)
     with open(path) as f:
         contents = f.read()
 
     if "title = " in contents:
         continue
-    contents = contents.replace("â€™", "'").replace("â€˜", "'").replace("â€”", " -- ")
-    # print(contents)
+
+    contents = re.sub(r"’", "'", contents)
+    contents = re.sub(r"“", '"', contents)
+    contents = re.sub(r"”", '"', contents)
+    contents = re.sub(r" ?— ?", " -- ", contents)
+    print(contents)
     # print()
 
+    print(r"(?P<name>.*?)\n"
+        r"(?P<level>\d)(\w\w)?-Level (?P<school>.*?) Spell \((?P<spell_lists>.*?)\)\n"
+        r"Casting Time: (?P<casting_time>.*?)\n"
+        r"Range: (?P<range>.*?)\n"
+        r"Components: (?P<components>.*?)( \((?P<material>.*?)\))?\n"
+        r"Duration: (?P<duration>.*?)\n"
+        r"(?P<description>.*)")
+
+    m = re.match(
+        r"(?P<name>.*?)\n"
+        r"(?P<level>\d)(\w\w)?-Level (?P<school>.*?) Spell \((?P<spell_lists>.*?)\)\n"
+        r"Casting Time: (?P<casting_time>.*?)\n"
+        r"Range: (?P<range>.*?)\n"
+        r"Components: (?P<components>.*?)( \((?P<material>.*?)\))?\n"
+        r"Duration: (?P<duration>.*?)\n"
+        r"(?P<description>.*)",
+        contents,
+        flags=re.DOTALL
+    )
+    print(m.groups())
+
     output = ""
-    name = basename(path)[:-5].replace('-', ' ').title()
-    name = name.replace(" S ", "'s ")
+
+    name = m["name"].replace(" ", "").title()
     output += f'title = "{name}"\n'
 
-    classes = [c for c in ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"]
-               if f"{c}_spell: {c}_spell" in contents]
-    output += "classes = " + str(classes).replace("'", '"') + "\n"
+    spell_lists = m['spell_lists'].split(", ")
+    output += f"spell_lists = {dumps(spell_lists)}\n"
 
-    m = search(r"level: (\d)", contents)
-    if m:
-        output += f'level = "{m.group(1)}"\n'
-    else:
-        if "level: cantrip" not in contents:
-            raise Exception("No spell level found")
-        output += f'level = "cantrip"\n'
+    output += f'level = "{m["level"]}"\n'
 
-    m = search(r"school: (.*?)_school", contents)
-    output += f'school = "{m.group(1)}"\n'
+    output += f'school = "{m["school"]}"\n'
 
-    concentration_spell = 'false' if "concentration_spell: '0'" in contents else 'true'
+    concentration_spell = "true" if "Concentration" in m["duration"] else "false"
     output += f"concentration_spell = {concentration_spell}\n"
 
-    ritual_spell = 'false' if "ritual_spell: '0'" in contents else 'true'
-    output += f"ritual_spell = {ritual_spell}\n"
+    output += "ritual_spell = false\n"
 
-    m = search(r"casting_time: (.*)", contents)
-    casting_time = m.group(1).strip("'")
+    casting_time = m["casting_time"].replace("\n", " ").strip(" ")
     output += f'casting_time = "{casting_time}"\n'
 
-    m = search(r"range: (.*)", contents)
-    range = m.group(1).strip("'")
-    output += f'range = "{range}"\n'
+    output += f'range = "{m["range"]}"\n'
 
-    components = [c[0].upper() for c in ["verbal", "somatic", "material"]
-                  if f"{c}_component: {c}_component" in contents]
-    output += "components = " + str(components).replace("'", '"') + "\n"
+    components = m['components'].split(", ")
+    output += f"components = {dumps(components)}\n"
 
     if "M" in components:
-        m = search(r"material_desc: (.*)", contents)
-        material = m.group(1).strip("'").replace("'", '"')
+        material = m["material"].replace("\n", " ").strip(" ")
         output += f'material = "{material}"\n'
 
-        m = search(r"expensive_material_component: '0'", contents)
-        output += f"expensive_material_component = {'false' if m else 'true'}\n"
+        output += f"expensive_material_component = {'true' if 'gp' in m['material'] else 'false'}\n"
 
-        m = search(r"material_component_consumed: '0'", contents)
-        output += f"material_component_consumed = {'false' if m else 'true'}\n"
+        output += f"material_component_consumed = {'true' if 'consumed' in m['material'] else 'false'}\n"
 
-    m = search(r"duration: (.*)", contents)
-    duration = m.group(1).strip("'")
+    if "Concentration" in m["duration"]:
+        duration = re.match(r"Concentration, up to (.*)", m["duration"]).group(1)
+    else:
+        duration = m["duration"]
     output += f'duration = "{duration}"\n'
 
-    m = search(r"description: (.*)", contents)
-    description = m.group(1).strip("'").strip('"').replace("''", "'").replace(r"\n", "\n").replace(r"\"", '"')
-    description.encode("ascii")
+    if "At Higher Levels." in m["description"]:
+        m2 = re.match(r"(.*?)At Higher Levels. (.*)", m["description"], flags=re.DOTALL)
+        description = m2.group(1)
+        at_higher_levels = m2.group(2)
+    else:
+        description = m["description"]
+        at_higher_levels = None
+
+    description = description.replace("\n", " ").strip(" ").replace("@", "  ")
     output += f'description = """{description}"""\n'
 
-    if r"at_higher_levels: at_higher_levels" in contents:
-        m = search(r"at_higher_levels_text: (.*)", contents)
-        at_higher_levels = m.group(1).strip("'").strip('"').replace("''", "'")
+    if at_higher_levels:
+        at_higher_levels = at_higher_levels.replace("\n", " ").strip(" ")
         output += f'at_higher_levels = "{at_higher_levels}"\n'
 
-    d = {
-        "phb": "Player's Handbook",
-        "dmg": "Dungeon Master's Guide",
-        "ee": "Elemental Evil",
-        "scag": "Sword Coast Adventurer's Guide",
-        "xgte": "Xanathar's Guide to Everything",
-        "llok": "Lost Laboratory of Kwalish",
-        "homebrew": "Homebrew"
-    }
-    source = search(r"sourcebook: (.*)", contents).group(1)
-    page = search(r"page: '(.*)'", contents).group(1)
-    if source == "homebrew":
-        output += f'source = "Homebrew"\n'
-    else:
-        output += f'source = "{d[source]}, p. {page}"\n'
+    output += f'source = "{source}"\n'
 
-    # print(output)
+    print(output)
 
     with open(path, 'w') as f:
         f.write(output)
     # break
+    print("-----------------")
