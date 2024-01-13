0e58ed51a7e86c02e49dce29bb44ca4cadb0cb4c
marco262 <marco262@gmail.com>
2024-01-13 11:39:01 -0800

    test_md_pages.py

--- tests/functional/test_md_pages.py
+++ tests/functional/test_md_pages.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from unittest import TestCase
 from unittest.mock import patch
 
@@ -19,12 +20,19 @@ class TestMdPages(TestCase):
         """
         Automatically builds all markdown pages to verify there are no errors thrown during conversion
         """
+        file_count = 0
         for dirname, subdirs, files in os.walk("data"):
             for filename in files:
+                file_count += 1
                 if not filename.endswith(".md"):
                     continue
                 split_dirs = dirname.replace("\\", "/").split("/", 2)
                 data_dir = split_dirs[1]
                 subdir = split_dirs[2] if len(split_dirs) > 2 else None
-                filename = os.path.splitext(filename)[0]
-                md_page(filename, namespace=data_dir, directory=subdir)
+                name = os.path.splitext(filename)[0]
+                try:
+                    md_page(name, namespace=data_dir, directory=subdir)
+                except Exception:
+                    print(f"Error when building markdown for file {os.path.join(dirname, filename)}", file=sys.stderr)
+                    raise
+        print(f"Validated {file_count} files")
